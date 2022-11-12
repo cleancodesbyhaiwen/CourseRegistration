e0c6988310c4aa31b3f5c242cecb3e7b5b68b33a
Haiwen Zhang <zhan3237@purdue.edu>
2022-11-11 19:08:17 -0500

    final

--- project1/project/routes.py
+++ project1/project/routes.py
@@ -4,8 +4,7 @@ from flask import request, render_template, g, redirect, url_for,flash
 from flask_login import login_user, logout_user, current_user, login_required
 from project.user import User
 
-
-is_student = None
+is_student = False
 
 @app.before_request
 def before_request():
@@ -71,7 +70,7 @@ def main():
     user = user.fetchone()
     # Getting all courses regitered by current user regardless of status with instructor information
     my_courses = g.conn.execute('''
-    SELECT Courses.name, Courses.c_id, Courses.course_header, Courses.college,Courses.credit,
+    SELECT Courses.id, Courses.name, Courses.c_id, Courses.course_header, Courses.college,Courses.credit,
     Make_registration.status, Courses.id,Instructors.name as instructor_name
     FROM Students, Make_registration, Courses,Instructors,Instruct
     WHERE Students.id='{}' and Students.id=Make_registration.student_id 
@@ -79,6 +78,19 @@ def main():
     and Instruct.course_id=Courses.id
     '''.format(current_user.get_id()))
     my_courses = my_courses.fetchall()
+
+    all_my_course_times = []
+    for my_course in my_courses:
+      my_course_times = g.conn.execute('''
+      SELECT Section_occur.day_of_week, Section_occur.begin_hour, Section_occur.begin_minute,
+      Section_occur.duration, Courses.course_header, Courses.c_id
+      FROM Courses,Section_occur
+      WHERE Courses.id={} and Section_occur.id=Courses.id
+      '''.format(my_course[0]))
+      my_course_times = my_course_times.fetchall()
+      for my_course_time in my_course_times:
+        all_my_course_times.append(my_course_time)
+    all_my_course_times = sorted(all_my_course_times)
 
     if request.method == 'POST':
       # Getting Search results
@@ -90,6 +102,8 @@ def main():
         elif searchby == 'ID':
           filter_ = "Courses.c_id LIKE '%%{}%%'".format(specifier)
         elif searchby == 'name':
+          filter_ = "Courses.name LIKE '%%{}%%'".format(specifier)
+        elif searchby == 'allCourses':
           filter_ = "Courses.name LIKE '%%{}%%'".format(specifier)
 
         courses = g.conn.execute('''
@@ -121,7 +135,8 @@ def main():
           enrolled_counts.append(enrolled_count)
 
         return render_template("student.html", user=user,courses=courses,my_courses=my_courses,
-        all_time_periods=all_time_periods,enrolled_counts=enrolled_counts)
+        all_time_periods=all_time_periods,enrolled_counts=enrolled_counts, search=True,
+        all_my_course_times=all_my_course_times)
 
       # Making a registration
       if request.form.get('join_waitlist'):
@@ -162,20 +177,25 @@ def main():
         flash('you successfully dropped the course')
         return redirect(url_for('main'))
         
-    return render_template("student.html", user=user,my_courses=my_courses)
+    return render_template("student.html", user=user,my_courses=my_courses,
+    all_my_course_times=all_my_course_times)
   ########################################################
   #                        Instructor                    #
   ########################################################
   else:
     # Get all courses taught by current user
     courses_teach = g.conn.execute('''
-          SELECT *
-          FROM Courses, Instruct
-          WHERE Courses.id=Instruct.course_id and Instruct.instructor_id={}
+          SELECT Courses.id, Courses.size, Courses.name, Courses.c_id, Courses.course_header, Instructors.name as instructor_name
+          , Classrooms.building_name, Classrooms.room
+          FROM Courses, Instruct, Instructors, Use_classroom, Classrooms
+          WHERE Courses.id=Instruct.course_id and Instruct.instructor_id={} and 
+          Instructors.id=Instruct.instructor_id and Use_classroom.course_id=Courses.id
+          and Use_classroom.classroom_id=Classrooms.id 
           '''.format(current_user.get_id()))
     courses_teach = courses_teach.fetchall()
     # Get number of enrolled students associated with each course
     enrolled_counts = []
+    all_time_periods = []
     for course in courses_teach:
       enrolled_count = g.conn.execute('''
       SELECT COUNT(*) 
@@ -184,6 +204,16 @@ def main():
       '''.format(course[0]))
       enrolled_count = enrolled_count.fetchone()
       enrolled_counts.append(enrolled_count[0])
+
+      time_periods = g.conn.execute('''
+      SELECT * 
+      FROM Courses,Section_occur
+      WHERE Courses.id={} and Section_occur.id=Courses.id
+      '''.format(course[0]))
+      time_periods = time_periods.fetchall()
+      all_time_periods.append(time_periods)
+
+
     # Get all registrations waiting to be considered
     waitlist_registrations = g.conn.execute('''
           SELECT Students.name, Students.college, Students.class_standing,Manage_registration.course_id
@@ -231,6 +261,7 @@ def main():
         '''.format(course_id,student_id))
       return redirect(url_for('main'))
 
+    # expel a student by delteing record from make_registration
     elif request.form.get('expel'):
       course_id = request.form.get('course_id')
       student_id = request.form.get('student_id')
@@ -241,5 +272,17 @@ def main():
         '''.format(course_id,student_id))
       return redirect(url_for('main'))
 
+    # Send a student back to waitlist by changing status to 'waitlist'
+    elif request.form.get('back_to_waitlist'):
+      course_id = request.form.get('course_id')
+      student_id = request.form.get('student_id')
+      g.conn.execute('''
+      UPDATE Make_registration
+      SET status='waitlist'
+      WHERE course_id={} and student_id={};
+        '''.format(course_id,student_id))
+      return redirect(url_for('main'))
+
     return render_template("instructor.html",waitlist_registrations=waitlist_registrations,
-    approved_registrations=approved_registrations,courses_teach=courses_teach,enrolled_counts=enrolled_counts)
+    approved_registrations=approved_registrations,courses_teach=courses_teach,enrolled_counts=enrolled_counts,
+    all_time_periods=all_time_periods)
